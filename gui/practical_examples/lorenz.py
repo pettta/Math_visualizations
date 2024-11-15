a66f5d4f1f7414b2d2672220fecab4d01fe24997
pettta <tp663@drexel.edu>
2024-11-15 11:41:06 -0500

    friday updates: adding more structure to basic system visuals

--- gui/practical_examples/lorenz.py
+++ gui/practical_examples/lorenz.py
@@ -1,6 +1,7 @@
 import numpy as np
-from scipy.integrate import solve_ivp
+import scipy
 import matplotlib.pyplot as plt
+import scipy.integrate
 from manim.mobject.opengl.opengl_vectorized_mobject import OpenGLVMobject
 from manim import * 
 
@@ -18,7 +19,7 @@ def lorenz_solver(initial_condition, t_span, sigma=10, rho=28, beta=8/3):
         dz_dt = x * y - beta * z
         return [dx_dt, dy_dt, dz_dt]
 
-    return solve_ivp(lorenz_system, t_span, initial_condition, dense_output=True).y.T
+    return scipy.integrate.solve_ivp(lorenz_system, t_span, initial_condition, dense_output=True).y.T
 
 
 class OpenGLShow(Scene):
